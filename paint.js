0d503e2d06d430342e86a1913249cd6e523dc794
krismac <email@krismac.com>
2018-10-16 15:34:26 +0100

    all files set up

---
+++ paint.js
@@ -0,0 +1,14 @@
+// A paint can should:
+//
+// have a number of litres of paint
+// be able to check if it is empty
+// be able to empty itself of paint
+
+
+const Paint = function() {
+
+
+
+
+
+};
---
+++ spec/room_spec.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const Room = require('../Room.js');
+const Paint = require('../Paint.js');
+const Decorator = require('../Decorator.js');
+
+
+// A room should:
+//
+// have an area in square meters
+// should start not painted
+// should be able to be painted
+
+
+const Room = function() {
+
+  beforeEach(function(){
+  paint = new Room("3");
+  //new instance of paint has (number of litres)
+  });
+
+  it('have an area in square meters', function(){
+
+  });
+
+  it('should start not painted', function(){
+
+  });
+
+  it('should be able to be painted', function(){
+
+  });
+
+
+
+};
